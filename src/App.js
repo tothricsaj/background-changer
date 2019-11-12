b867f5bc808fb8eac3510fd291feb6bec9bca3d7
tothricsaj <tothricsaj@gmail.com>
2019-11-12 12:42:41 +0100

    :art: Develop
    hide/show codechanger component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,61 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <h2 className="title">
-            Background changer app
-        </h2>
+class ColorChanger extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-        <div className="frame">
-            <div className="content"></div>
-        </div>
-    </div>
-  );
+    render() {
+        return (
+            <div className="changer-wrapper">
+                Kiscica füle
+            </div>
+        );
+    }
 }
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showChanger: false,
+        }
+
+        this.toggleChanger = this.toggleChanger.bind(this);
+    }
+
+    toggleChanger() {
+        this.setState({showChanger: !this.state.showChanger});
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h2 className="title">
+                    Background changer app
+                </h2>
+
+                <div className="frame">
+                    <div className="content">
+                        <div 
+                            className="colorChanger"
+                            onClick={this.toggleChanger}
+                        >
+                            Change the color
+                        </div>
+
+                        {this.state.showChanger &&
+                            <ColorChanger />
+                        }
+
+                    </div>
+                </div>
+            </div>
+         );
+
+    }
+  }
 
 export default App;
