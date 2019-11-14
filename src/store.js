2a9e466022812756a482c080b2f2a9c3cbce9360
tothricsaj <tothricsaj@gmail.com>
2019-11-14 15:55:15 +0100

    :zip: store config

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux';
+import ColorSetterReducer from './reducers/ColorSetterReducer.js';
+
+const defaultStore = {
+    marginLeft: 0,
+    bgColor: '#0c7c59',
+    frameColor: '#bac1b8',
+};
+
+function configStore(store = defaultStore) {
+    createStore(ColorSetterReducer, state);
+}
