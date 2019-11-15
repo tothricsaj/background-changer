0ef4767a398eefed77a3e3b8ab00fe4e53c4ecda
tothricsaj <tothricsaj@gmail.com>
2019-11-15 16:08:17 +0100

    App is a class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { msgAction } from './actions/msgAction.js';
 
-function App() {
-  return (
-    <div className="App">
-        <h2>Redux-React basics</h2>
-    </div>
-  );
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="App">
+            <h2>Redux-React basics</h2>
+        </div>
+        );
+      }
 }
 
 export default App;
